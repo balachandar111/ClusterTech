bd1ce9aa8f93817cd17303de5e842eadecbc01e8
balachandar111 <balachandarsanthoshkumar111.com>
2025-01-24 11:13:46 +0530

    Initial commit

---
+++ clusterapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def nav(request):
+    return render(request,"nav.html")
+def home(request):
+    return render(request,"pages/home.html")
